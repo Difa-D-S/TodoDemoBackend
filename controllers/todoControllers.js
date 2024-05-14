faa93ac6b06dcf06a4764a14638b06bad713681e
Difa-D-S <difads123@gmail.com>
2024-05-14 11:53:17 +0530

    first todo backend demo

---
+++ controllers/todoControllers.js
@@ -0,0 +1,37 @@
+import Todo from '../model/TodoModel.js'
+
+const getTodo = async(req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+}
+
+const addTodo = async(req,res) => {
+    console.log(req.body)
+
+    const todo = await Todo.create({
+      title: req.body.title,
+      desc: req.body.desc,
+    });
+    
+    res.json(todo);
+  }
+
+const deleteTodo = async(req, res) => {
+    console.log(req.params);
+    await Todo.findByIdAndDelete(req.params.id);
+    res.status(200).send("Deleted");
+}
+
+const updateTodo = async(req, res) => {
+    const {title, desc} = req.body;
+
+    const todo = await Todo.findById(req.params.id);
+
+    todo.title = title || todo.title;
+    todo.desc = desc || todo.desc;
+
+    const updatedTodo = await todo.save();
+    res.json(updatedTodo);
+}
+
+export {getTodo, addTodo, deleteTodo, updateTodo}
